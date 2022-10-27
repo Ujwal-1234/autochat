dfb02bbf0ac0c2d78b8335841dddcfd61a23e9ea
Ujwal-1234 <56074956+Ujwal-1234@users.noreply.github.com>
2022-10-27 09:29:42 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Real Time Chat</title>
+        <title>Autochat</title>
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     </head>
