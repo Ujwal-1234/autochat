672098b951ff9cd252d29f754a25cfb1b216137b
Ujwal-1234 <56074956+Ujwal-1234@users.noreply.github.com>
2022-10-27 09:32:03 +0530

    Update header.php

--- header.php
+++ header.php
@@ -2,6 +2,6 @@
     <head>
         <title>Autochat</title>
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+        <link rel="stylesheet" href="_style.css">
     </head>
 </html>
